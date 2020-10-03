08189948a9bb029e2cc34a068b28e6403492c4b6
zura0z <zuramazmishvili@gmail.com>
2020-10-03 13:50:13 +0400

    Updates

---
+++ precache-manifest.9b220beb47b2596a8d94e54703ac8613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fec280bdf94d686e31c8d10a92d6af9",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "95da646d2b9fc142faca",
+    "url": "/calculator/static/css/main.5172f65f.chunk.css"
+  },
+  {
+    "revision": "3ba8ce8455d274225817",
+    "url": "/calculator/static/js/2.28d684e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.28d684e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95da646d2b9fc142faca",
+    "url": "/calculator/static/js/main.2ded2c3b.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
